d64863889d87c0dbc4a60688f9fe9c4c12f4dc71
Adriano Dias <adriano.dsm1000@gmail.com>
2024-02-21 21:31:42 -0300

    feat: getEmployeesCoverage finished

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
+const employeeList = data.employees;
+const speciesList = data.species;
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function employeeSheetFactory(){
+  const employeeSheet = employeeList.map(e=>({
+    id: e.id,
+    fullName: e.firstName + ' ' + e.lastName,
+    species: e.responsibleFor.map(element=>speciesList.find(i=>i.id===element).name),
+    locations: e.responsibleFor.map(element=>speciesList.find(i=>i.id===element).location)
+  }))
+  return employeeSheet
 }
 
-module.exports = getEmployeesCoverage;
+
+function getEmployeesCoverage(employeeData) {
+  const employeeSheet = employeeSheetFactory()
+ if(!employeeData){
+  return employeeSheet
+ }else if(employeeList.some(e=>e.id===employeeData.id) || employeeList.some(e=>e.firstName===employeeData.name) || employeeList.some(e=>e.lastName===employeeData.name)){
+    if(employeeSheet.some(e=>e.id === employeeData.id)){
+      return employeeSheet.find(e=>e.id===employeeData.id)
+    }else if(employeeSheet.some(e=>e.fullName.includes(employeeData.name))){
+      return employeeSheet.find(e=>e.fullName.includes(employeeData.name))
+    }
+  }else{
+    throw new Error('Informações inválidas');
+  }
+}
+
+
+console.log('getEmployeesCoverage',getEmployeesCoverage({name: 'Nigel'}))
+module.exports = getEmployeesCoverage, employeeSheetFactory;
